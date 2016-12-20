aa7ce4dbf199c74ab9967fdb2e20c90d7bc4ed89
Van Donley <vandonley@gmail.com>
2016-12-19 16:47:17 -0800

    Parsec Account
    Create local Parsec account and fix settings of local built-in
    administrator account.

---
+++ LocalUsers_DSC.ps1
@@ -0,0 +1,31 @@
+﻿Configuration LocalUsers
+{
+ Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+
+   node localhost
+   {
+    User 'Parsec_Admin'
+    {
+     UserName = 'Parsec'
+     Description = 'Support Account'
+     Disabled = $false
+     Ensure = 'Present'
+     FullName = 'Parsec Computer Corp.'
+     PasswordChangeNotAllowed = $false
+     PasswordChangeRequired = $false
+     PasswordNeverExpires = $true
+    }
+    User 'Local_Admin'
+    {
+     UserName = 'Administrator'
+     DependsOn = '[User]Parsec_Admin'
+     Description = 'Built-in account for administering the computer/domain'
+     Disabled = $true
+     Ensure = 'Present'
+     PasswordChangeNotAllowed = $false
+     PasswordChangeRequired = $false
+     PasswordNeverExpires = $true
+    }
+
+LocalUsers
+Start-DscConfiguration -Path .\LocalUsers -Wait -Force
